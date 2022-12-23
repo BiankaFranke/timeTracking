1d3cce2720d18941a35362c16ece6cbae5c584ee
“BiankaFranke” <bianka_franke@hotmail.de>
2022-12-23 12:49:10 +0100

    first js parts added

---
+++ assets/js/main.js
@@ -0,0 +1,136 @@
+console.log('JS check')
+
+// Declaration
+const daily = document.querySelector("#daily")
+const weekly = document.querySelector("#weekly")
+const monthly = document.querySelector("#monthly")
+
+// Data
+
+const timeframe = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ]
+
+// Eventlistener
+
+daily.addEventListener('click', () => {
+    console.log('test')
+    daily.classList.add('active')
+    weekly.classList.remove('active')
+    monthly.classList.remove('active')
+})
+
+weekly.addEventListener('click', () => {
+    console.log('test2')
+    weekly.classList.add('active')
+    daily.classList.remove('active')
+    monthly.classList.remove('active')
+})
+
+monthly.addEventListener('click', () => {
+    console.log('test3')
+    monthly.classList.add('active')
+    weekly.classList.remove('active')
+    daily.classList.remove('active')
+})
